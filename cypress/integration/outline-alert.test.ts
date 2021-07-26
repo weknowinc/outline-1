bc0acb719bbbc66e8b15bd9c934709ad803bbdc2
Dan Montgomery <dmontgomery@phase2technology.com>
2021-07-26 09:24:17 -0500

    test: Update Cypress test to use Storybook.

--- cypress/integration/outline-alert.test.ts
+++ cypress/integration/outline-alert.test.ts
@@ -1,27 +1,20 @@
 describe('Outline Alert', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8000/cypress/fixtures/empty-html');
+    // This URL is slightly different that the storybook page.
+    // Swap `?path=/story/` with `iframe.html?id=`.
+    // This loads just the component iframe.
+    cy.visit('http://localhost:6006/iframe.html?id=molecules-alert--default-example');
   });
 
-  it('Add simple alert', () => {
-    // This is done inside this wrapper so we are using the document from the application iFrame. 
-    cy.document().then( document => {
-      const alertElement = document.createElement('outline-alert');
-
-      const testMessage = 'Test message';
-      const messageElement = document.createElement('p');
-      messageElement.classList.add('message');
-
-      messageElement.innerText = testMessage;
-      alertElement.append(messageElement);
-
-      const body = document.querySelector('body') as HTMLBodyElement;
-      body.append(alertElement);
-
-      // If we use a cy.* method, we will let Cypress retry the `expect` a few times.
+  it('Default slot (message)', () => {
+    // If we use a cy.* method, we will let Cypress retry the `expect` a few times.
       // This is helpful because we wait for the element to render.
-      cy.get('.message').should(($messageElement) => {
-        expect($messageElement.prop('assignedSlot'), 'Message is not slotted.').is.not.null;
+    cy.get('outline-alert > *').should(($messageElements) => {
+      expect($messageElements.length, "Elements in the default slot").to.be.greaterThan(0);
+      
+      // We use `function() {}` instead of `() => {}` so we can access `this`.
+      $messageElements.each(function() {
+        expect(this.assignedSlot, "Message is slotted.").is.not.null.is.not.undefined;
       });
     });
   });
